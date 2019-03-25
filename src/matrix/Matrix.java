5a963f86b7a6dcee55195bdc6440b4dc36fcfa50
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-03-25 08:12:11 +0530

    Implement Matrix with basic addition and subtraction features

---
+++ src/matrix/Matrix.java
@@ -0,0 +1,75 @@
+package matrix;
+
+class Matrix {
+
+    private interface ArithmeticOperation{
+        Integer perform(Integer a, Integer b);
+    }
+
+    private Integer[][] matrixData;
+
+    Matrix(Integer[][] matrixData) {
+        this.matrixData = matrixData.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Matrix matrix = (Matrix) o;
+        return isEqualTo(matrix);
+    }
+
+    private boolean isEqualTo(Matrix otherMatrix) {
+        if (this.matrixData.length != otherMatrix.matrixData.length) return true;
+
+        for (int rowIndex = 0; rowIndex < this.matrixData.length; rowIndex++) {
+            Integer[] myRow = this.matrixData[rowIndex];
+            Integer[] otherRow = otherMatrix.matrixData[rowIndex];
+            if (myRow.length != otherRow.length) return false;
+            for (int colIndex = 0; colIndex < myRow.length; colIndex++) {
+                if (this.matrixData[rowIndex][colIndex] != otherMatrix.matrixData[rowIndex][colIndex]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Matrix performOperation(Matrix otherMatrix, ArithmeticOperation operation) {
+        Integer[][] resultMatrixData = new Integer[this.matrixData.length][];
+        for (int rowIndex = 0; rowIndex < this.matrixData.length; rowIndex++) {
+            Integer[] row = this.matrixData[rowIndex];
+            resultMatrixData[rowIndex] = new Integer[row.length];
+            for (int colIndex = 0; colIndex < row.length; colIndex++) {
+                Integer thisElement = this.matrixData[rowIndex][colIndex];
+                Integer thatElement = otherMatrix.matrixData[rowIndex][colIndex];
+                int resultedElement = operation.perform(thisElement, thatElement);
+                resultMatrixData[rowIndex][colIndex] = resultedElement;
+            }
+        }
+
+        return new Matrix(resultMatrixData);
+    }
+
+    Matrix add(Matrix otherMatrix) {
+        return performOperation(otherMatrix, (a, b) -> a+b);
+    }
+
+
+    Matrix sub(Matrix otherMatrix) {
+        return performOperation(otherMatrix, (a, b) -> a-b);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Integer[] row : this.matrixData) {
+            for (Integer element : row) {
+                result.append(element).append(' ');
+            }
+            result.append('\n');
+        }
+        return result.toString();
+    }
+}
---
+++ test/matrix/MatrixTest.java
@@ -0,0 +1,43 @@
+package matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @Test
+    void shouldAssertThatTwoMatricesAreEqual() {
+        Integer[][] matrixData = {{1, 2}, {3, 4}, {5, 6}};
+        Matrix firstMatrix = new Matrix(matrixData);
+        Matrix secondMatrix = new Matrix(matrixData);
+
+        assertEquals(firstMatrix, secondMatrix);
+    }
+
+    @Test
+    void shouldReturnAdditionOfTwoMatricesOfSameSize() {
+        Integer[][] firstMatrixData = {{1, 2}, {3, 4}, {5, 6}};
+        Integer[][] secondMatrixData = {{1, 1}, {1, 1}, {1, 1}};
+        Integer[][] expectedMatrixData = {{2, 3}, {4, 5}, {6, 7}};
+
+        Matrix firstMatrix = new Matrix(firstMatrixData);
+        Matrix secondMatrix = new Matrix(secondMatrixData);
+        Matrix expectedMatrix = new Matrix(expectedMatrixData);
+
+        assertEquals(expectedMatrix, firstMatrix.add(secondMatrix));
+    }
+
+    @Test
+    void shouldReturnSubtractionOfTwoMatricesOfSameSize() {
+        Integer[][] firstMatrixData = {{1, 2}, {3, 4}, {5, 6}};
+        Integer[][] secondMatrixData = {{1, 1}, {1, 1}, {1, 1}};
+        Integer[][] expectedMatrixData = {{0, 1}, {2, 3}, {4, 5}};
+
+        Matrix firstMatrix = new Matrix(firstMatrixData);
+        Matrix secondMatrix = new Matrix(secondMatrixData);
+        Matrix expectedMatrix = new Matrix(expectedMatrixData);
+
+        assertEquals(expectedMatrix, firstMatrix.sub(secondMatrix));
+    }
+}
